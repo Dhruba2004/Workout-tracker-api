9d64a9ae4e655485889bd5144a44a0e7f65e50ca
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-03-09 03:00:06 +0530

    :(

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
-const corsOptions = require('./config/corsOptions.js')
+const corsOptions = require('./config/corsOptions')
 const cors = require('cors')
 
 // express app
